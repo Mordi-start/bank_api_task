ac29a1a301ff87c97381f9f6ecb4b34f0433fd50
Морель Дмитрий Алексеевич <DAMorel@sberbank.ru>
2021-05-14 10:59:45 +0300

    Initial Commit

---
+++ src/main/java/ru/dmitrymorel/bank_api_task/Main.java
@@ -0,0 +1,7 @@
+package ru.dmitrymorel.bank_api_task;
+
+public class Main {
+    public static void main(String[] args) {
+
+    }
+}
---
+++ src/main/java/ru/dmitrymorel/bank_api_task/service/UserService.java
@@ -0,0 +1,4 @@
+package ru.dmitrymorel.bank_api_task.service;
+
+public interface UserService {
+}
